c206cf1ad9d4bb19fe37e1ca921964b53e6b5a71
Yann Gasner <yann.gasner@gmail.com>
2026-06-14 16:40:34 +0200

    package upgrade

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './Components/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />,
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />
+  </React.StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
